0dfd85d72a439dccf71ebde6f8938a7f6f7eb256
gaoussoukebe <33683408+gaoussoukebe@users.noreply.github.com>
2020-04-29 04:10:18 -0400

    Update run_pretraining_nodocker.sh

--- scripts/run_pretraining_nodocker.sh
+++ scripts/run_pretraining_nodocker.sh
@@ -28,7 +28,7 @@ gradient_accumulation_steps=256
 gradient_accumulation_steps_phase2=512
 resume_training="true"
 
-while getopts g:p:c:n:d:e:x:y:a:b:w:z option 
+while getopts g:p:c:n:d:e:x:y:a:b:w:z:r option 
 do 
  case "${option}" 
  in 
