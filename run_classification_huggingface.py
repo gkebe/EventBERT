43107e197b0d68ed7b1ac4ebcba7a688eef67612
gaoussoukebe <33683408+gaoussoukebe@users.noreply.github.com>
2020-05-05 04:31:57 -0400

    Huggingface load model

--- run_classification_huggingface.py
+++ run_classification_huggingface.py
@@ -612,13 +612,15 @@ def main():
         if args.local_rank != -1:
             num_train_optimization_steps = num_train_optimization_steps // torch.distributed.get_world_size()
 
-    # Prepare model
-    config = BertConfig.from_json_file(args.config_file)
-    # Padding for divisibility by 8
+
+    config = BertConfig(num_labels=num_labels)
     if config.vocab_size % 8 != 0:
         config.vocab_size += 8 - (config.vocab_size % 8)
-
-    model = BertForSequenceClassification.from_pretrained("bert-base-uncased", num_labels=num_labels)
+    model_fn = args.init_checkpoint
+    bert_model = 'bert-base-uncased'    
+    model_state_dict = torch.load(model_fn, map_location='cpu')["model"]
+    model = BertForSequenceClassification.from_pretrained(bert_model, config = config, state_dict = model_state_dict)
+    print(model.num_labels)
     # print("USING CHECKPOINT from", args.init_checkpoint)
     # model.load_state_dict(torch.load(args.init_checkpoint, map_location='cpu')["model"], strict=False)
     # print("USED CHECKPOINT from", args.init_checkpoint)
