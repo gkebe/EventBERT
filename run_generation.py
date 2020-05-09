9f44651bf32387833b29e7ade01f01c30998059f
gaoussoukebe <33683408+gaoussoukebe@users.noreply.github.com>
2020-05-09 12:22:15 -0400

    Update run_generation.py

--- run_generation.py
+++ run_generation.py
@@ -198,7 +198,9 @@ def main():
         batch = get_init_sequence(seed_text, max_len, batch_size, seq_len)
 
         for ii in range(max_iter):
+            print(batch[0])
             mask_indices = [i for i in range(len(batch[0])) if batch[0][i] == MASK]
+            print(mask_indices)
             kk = mask_indices[np.random.randint(seed_len, len(mask_indices))] - seed_len
             for jj in range(batch_size):
                 batch[jj][seed_len + kk] = mask_id
