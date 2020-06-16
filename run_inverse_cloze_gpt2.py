a9ad95446a1cbcb4eb3c185bb997eb74b74b07f3
gaoussoukebe <33683408+gaoussoukebe@users.noreply.github.com>
2020-06-16 06:51:17 -0400

    Update run_inverse_cloze_gpt2.py

--- run_inverse_cloze_gpt2.py
+++ run_inverse_cloze_gpt2.py
@@ -39,7 +39,6 @@ from modeling import BertForNextSentencePrediction, BertConfig, WEIGHTS_NAME, CO
 from tokenization import BertTokenizer
 from optimization import BertAdam, warmup_linear
 from schedulers import LinearWarmUpScheduler
-from apex import amp
 
 from transformers import GPT2Tokenizer, GPT2Config, GPT2LMHeadModel
 from sklearn.metrics import matthews_corrcoef, f1_score, recall_score, precision_score, classification_report, \
@@ -235,34 +234,6 @@ def convert_instances_to_features(instances, max_seq_length, tokenizer):
 def accuracy(out, labels):
     outputs = np.argmax(out, axis=1)
     return np.sum(outputs == labels)
-
-
-from apex.multi_tensor_apply import multi_tensor_applier
-
-
-class GradientClipper:
-    """
-    Clips gradient norm of an iterable of parameters.
-    """
-
-    def __init__(self, max_grad_norm):
-        self.max_norm = max_grad_norm
-        if multi_tensor_applier.available:
-            import amp_C
-            self._overflow_buf = torch.cuda.IntTensor([0])
-            self.multi_tensor_l2norm = amp_C.multi_tensor_l2norm
-            self.multi_tensor_scale = amp_C.multi_tensor_scale
-        else:
-            raise RuntimeError('Gradient clipping requires cuda extensions')
-
-    def step(self, parameters):
-        l = [p.grad for p in parameters if p.grad is not None]
-        total_norm, _ = multi_tensor_applier(self.multi_tensor_l2norm, self._overflow_buf, [l], False)
-        total_norm = total_norm.item()
-        if (total_norm == float('inf')): return
-        clip_coef = self.max_norm / (total_norm + 1e-6)
-        if clip_coef < 1:
-            multi_tensor_applier(self.multi_tensor_scale, self._overflow_buf, [l, l], clip_coef)
 
 def perplexity(tokenizer, model, full, device):
     input_ids = torch.tensor(tokenizer.encode(full)).unsqueeze(0)
