9a895f4f83ff3ab3331af04b8a25e9d7e6edafcb
gaoussoukebe <33683408+gaoussoukebe@users.noreply.github.com>
2020-06-25 22:11:19 -0400

    Update run_generation_bert.py

--- run_generation_bert.py
+++ run_generation_bert.py
@@ -101,6 +101,7 @@ def main():
     cls_id = tokenizer.convert_tokens_to_ids([CLS])[0]
 
     def tokenize_batch(batch):
+        print(batch)
         tokens = [tokenizer.tokenize(i) for i in batch]
         return [tokenizer.convert_tokens_to_ids(sent) for sent in tokens]
 
