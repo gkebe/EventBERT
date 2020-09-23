c24526691b836166fd492b7544e9d25b0e043b7f
gaoussoukebe <33683408+gaoussoukebe@users.noreply.github.com>
2020-09-23 14:34:11 -0400

    Update get_cloze_results.py

--- get_cloze_results.py
+++ get_cloze_results.py
@@ -37,11 +37,11 @@ def main():
                         help="The input data dir. Should contain the .tsv files (or other data files) for the task.")
     args = parser.parse_args()
     for file in list(glob.glob('results/'+args.dataset+'/eval_results_alt_ckpt_*_cloze_dataset_weber.txt')):
-        epoch = float(re.findall("\d+", file)[0])
+        epoch = int(re.findall("\d+", file)[0])
         with open(file, "r") as f:
             text = f.read()
-        accuracy = float(re.findall("\d+",re.findall("accuracy = \d+", text)[0])[0])
-        mrr = float(re.findall("\d+", re.findall("MRR = \d+", text)[0])[0])
+        accuracy = float(re.findall("\d+\.\d+",re.findall("accuracy = \d+\.\d+", text)[0])[0])
+        mrr = float(re.findall("\d+\.\d+", re.findall("MRR = \d+\.\d+", text)[0])[0])
         print(epoch, accuracy, mrr)
 if __name__ == "__main__":
     main()
