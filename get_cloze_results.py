f7a6aad86bc96c7a3310335498056760ade25125
gaoussoukebe <33683408+gaoussoukebe@users.noreply.github.com>
2020-09-23 23:41:43 -0400

    Update get_cloze_results.py

--- get_cloze_results.py
+++ get_cloze_results.py
@@ -37,6 +37,7 @@ def main():
                         help="The input data dir. Should contain the .tsv files (or other data files) for the task.")
     args = parser.parse_args()
     results =dict()
+    print(list(glob.glob('results/'+args.dataset+'/eval_results_alt_ckpt_*_cloze_dataset_weber.txt')))
     for file in list(glob.glob('results/'+args.dataset+'/eval_results_alt_ckpt_*_cloze_dataset_weber.txt')):
         instances = int(re.findall("\d+", file)[0])*81920
         with open(file, "r") as f:
