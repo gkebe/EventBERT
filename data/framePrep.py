52aeb49948e63e4220e987641b78acec6bbe0b72
gaoussoukebe <33683408+gaoussoukebe@users.noreply.github.com>
2020-11-05 04:25:14 -0500

    Update framePrep.py

--- data/framePrep.py
+++ data/framePrep.py
@@ -10,7 +10,7 @@ def wiki70k_preprocess(filename, tuple_to_sen = False, keep_label = False, add_t
   paragraphs_articles = paragraphs.split("\n")
   paragraphs_lst = [list(i.split("<PARAGRAPH>")[1:]) for i in paragraphs_articles]
   paragraphs_lst_words = [[list(j.split(" ")) for j in i] for i in paragraphs_lst][:-1]
-  paragraph_lst_sentences = ''.join([''.join([', '.join([' '.join(v[i:i + 5][4:5]) for i in range(0, len(v), 5)][:-1]) + "\n" for v in j]) for j in paragraphs_lst_words])
+  paragraph_lst_sentences = ''.join([''.join([' '.join([' '.join(v[i:i + 5][4:5]) for i in range(0, len(v), 5)][:-1]) + "\n" for v in j]) for j in paragraphs_lst_words])
   f.close()
   print("Articles: "+ str(sum([len(i) for i in paragraphs_lst_words])))
   print("Sentences: "+ str(sum([sum([len(j) for j in i]) for i in paragraphs_lst_words])))
